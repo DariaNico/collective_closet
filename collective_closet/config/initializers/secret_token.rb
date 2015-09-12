62c228fe509511dc9fb91e00e2bac95da7074b4d
Cole Spanierman <colespan@gmail.com>
2015-09-12 10:20:09 -0500

    Generated new rails app [DT]

---
+++ collective_closet/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CollectiveCloset::Application.config.secret_key_base = '28203bde257cc1062bc026c9c6227570877606d94381179f7c23c06fb836a88f552e1a34ea4e02e7dd3e1796c108d7ae90c7fcdcc0502a0985bf71aa021dd807'
---
+++ collective_closet/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CollectiveCloset::Application.config.session_store :cookie_store, key: '_collective_closet_session'
